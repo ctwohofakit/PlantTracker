1bfd576bbd5e62b9ad2ab1163f5a8fa1affe9283
kit sitou <ctwohofa@gmail.com>
2026-05-06 06:41:09 -0700

    detail view adjustment

--- PlantTracker/Views/ContentView.swift
+++ PlantTracker/Views/ContentView.swift
@@ -21,151 +21,133 @@ struct ContentView: View {
                     .resizable()
                     .ignoresSafeArea()
                 
-                VStack{
-                    //MARK: Top display---
+                ZStack{
+              
                     
                     VStack{
-//                        Text("Plant Care")
-//                            .font(.title2).bold()
-                        Spacer()
-                    }.frame(height:50)
-                    
-                    
-                    
-                    //MARK: catergory & list card---
-                    VStack{
+                        //MARK: Top display---
+                        
+                        Spacer().frame(height:60)
+                        
+                        
+                        
+                        //MARK: catergory & list card---
                         VStack{
-                            HStack{
-                                Text("Categories")
-                                    .font(.title)
-                                    
-                                Spacer()
-                                Text("See all")
-                                
-                            }.foregroundStyle(.dateLabel)
-                            //end of Vstack
-                            .padding(.vertical).bold()
-                            ScrollView(.horizontal, showsIndicators: false){
-                                HStack(spacing: 20){
-                                    VStack{
-                                        Image(systemName: "camera.macro")
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(width:25, height:25)
-                                            .padding()
-                                            .background(Color("background"))
-                                            .clipShape(.circle)
-                                            .foregroundStyle(.white)
-                                        Text("Flower")
-                                            .font(.headline).bold()
+                            /*
+                             VStack{
+                             HStack{
+                             Text("Categories")
+                             .font(.title)
+                             
+                             Spacer()
+                             Text("See all")
+                             
+                             }.foregroundStyle(.dateLabel)
+                             //end of Vstack
+                             .padding(.vertical).bold()
+                             ScrollView(.horizontal, showsIndicators: false){
+                             HStack(spacing: 20){
+                             VStack{
+                             Image(systemName: "camera.macro")
+                             .resizable()
+                             .scaledToFit()
+                             .frame(width:25, height:25)
+                             .padding()
+                             .background(Color("background"))
+                             .clipShape(.circle)
+                             .foregroundStyle(.white)
+                             Text("Flower")
+                             .font(.headline).bold()
+                             
+                             }.frame(width:100, height: 100)
+                             .background(.white)
+                             .cornerRadius(15)
+                             
+                             
+                             
+                             
+                             
+                             
+                             VStack{
+                             Image(systemName: "carrot.fill")
+                             .resizable()
+                             .scaledToFit()
+                             .frame(width:25, height:25)
+                             .padding()
+                             .background(Color("background"))
+                             .clipShape(.circle)
+                             .foregroundStyle(.white)
+                             Text("Fruit")
+                             .font(.headline).bold()
+                             
+                             }.frame(width:100, height: 100)
+                             .background(.white)
+                             .cornerRadius(15)
+                             VStack{
+                             Image(systemName: "apple.meditate")
+                             .resizable()
+                             .scaledToFit()
+                             .frame(width:25, height:25)
+                             .padding()
+                             .background(Color("background"))
+                             .clipShape(.circle)
+                             .foregroundStyle(.white)
+                             Text("Plant")
+                             .font(.headline).bold()
+                             
+                             }.frame(width:100, height: 100)
+                             .background(.white)
+                             .cornerRadius(15)
+                             
+                             
+                             //                                    CategoryCard(icon:)
+                             }
+                             
+                             }
+                             }.shadow(color: .gray.opacity(0.3), radius: 3, x: 5, y: 2)
+                             .padding(.horizontal,30)
+                             */
+                            //End: Catergories
+                            //                        VStack(alignment: .leading){
+                            //                            }
+                            
+                            VStack(alignment: .leading, spacing: 0){
+                                Text("My Plants")
+                                    .font(.title).bold()
+                                    .foregroundStyle(.dateLabel)
+                                    .padding(.leading, 30)
+                                    .padding(.top, 20)
+                                List($plants){ plant in
+                                    NavigationLink(destination: PlantDetailsView(plant: plant)){
+                                        PlantListItem(plant: plant.wrappedValue)
                                         
-                                    }.frame(width:100, height: 100)
-                                     .background(.white)
-                                     .cornerRadius(15)
-                                     
-                                    
-                                     
-
-                                    
-                                    
-                                    VStack{
-                                        Image(systemName: "carrot.fill")
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(width:25, height:25)
-                                            .padding()
-                                            .background(Color("background"))
-                                            .clipShape(.circle)
-                                            .foregroundStyle(.white)
-                                        Text("Fruit")
-                                            .font(.headline).bold()
-                                        
-                                    }.frame(width:100, height: 100)
-                                     .background(.white)
-                                     .cornerRadius(15)
-                                    VStack{
-                                        Image(systemName: "apple.meditate")
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(width:25, height:25)
-                                            .padding()
-                                            .background(Color("background"))
-                                            .clipShape(.circle)
-                                            .foregroundStyle(.white)
-                                        Text("Plant")
-                                            .font(.headline).bold()
-                                        
-                                    }.frame(width:100, height: 100)
-                                     .background(.white)
-                                     .cornerRadius(15)
-                                    
-                                    
-                                    //                                    CategoryCard(icon:)
+                                    } .listRowBackground(Color.clear)
+                                        .listRowSeparator(.hidden)
+                                        .listStyle(.plain)
                                 }
                                 
                             }
-                        }.shadow(color: .gray.opacity(0.3), radius: 3, x: 5, y: 2)
-                        .padding(.horizontal,30)//End: Catergories
-//                        VStack(alignment: .leading){
-//                            }
-                        VStack(alignment: .leading, spacing: 0){
-                            Text("My Plants")
-                                .font(.title).bold()
-                                .foregroundStyle(.dateLabel)
-                                .padding(.leading, 30)
-                                .padding(.top, 10)
-                                .padding(.bottom, 0)
-                            List($plants){plant in
-                                NavigationLink(destination: PlantDetailsView(plant: plant)){
-                                    PlantListItem(plant: plant.wrappedValue)
-                                    
-                                } .listRowBackground(Color.clear)
-                                    .listRowSeparator(.hidden)
-                                    .listStyle(.plain)
-                                    
-                                
-                                
+                            .listRowBackground(Color("listBackground"))
                             
                             
-//                            ScrollView(.vertical, showsIndicators: false){
-//                                ForEach(plants){ plant in
-//                                    PlantListItem(plant: plant)
-//                                
-//                                }
-                            }
-//                                .listRowInsets(EdgeInsets())
-                              
-                              
-                                    //                            ForEach(specialistList){ specialist in
-                                    //                            SpecialistCardView(specialist: specialist)
-                                    //
-                              
-                                
-                               
-                           
-                        }
-                        .listRowBackground(Color("listBackground"))
-//                        .padding(5)
+                            
+                        }//MARK: END_mid to bottom Vstack
+                        .scrollContentBackground(.hidden)
+                        .background(
+                            UnevenRoundedRectangle(topLeadingRadius: 50, topTrailingRadius: 50).fill(Color("listBackground"))
+                                .frame(maxWidth:.infinity)
+                        ).ignoresSafeArea()
                         
                         
                         
-                    }//MARK: END_mid to bottom Vstack
-                    .scrollContentBackground(.hidden)
-                    .padding(.vertical, 20)
-                    .frame(maxWidth:.infinity,maxHeight:.infinity)
-                    .background(Color("listBackground"))
-                    .clipShape(
-                        .rect(topLeadingRadius: 60,topTrailingRadius: 60 ))
-                    .ignoresSafeArea()
-                    
-                    //                    .background("background")
-                    //                        .ignoresSafeArea()
-                }//end:main Vstack
-                
-            }//END: Z
+                        
+                    }
+                    //end:main Vstack
+                }
+            } //END: Z
             .navigationTitle("Plant Care")
             .navigationBarTitleDisplayMode(.inline)
-            .navigationBarItems(trailing: Button("Add View"){
+            .navigationBarItems(trailing: Button("Add"){
                 showAddPlant.toggle()
             })
             .sheet(isPresented: $showAddPlant){
--- PlantTracker/Views/PlantDetailsView.swift
+++ PlantTracker/Views/PlantDetailsView.swift
@@ -10,48 +10,192 @@ import SwiftUI
 struct PlantDetailsView: View {
     @Binding var plant: Plant
     
+    @State private var showEditPlant: Bool = false
+    
     var body: some View {
         VStack(alignment: .leading){
-            HStack{
-                Image(plant.plantImage)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:100, height:150)
+            NavigationStack{
+       
+            ZStack(alignment: .bottom){
+                Color("listBackground")
+                    .ignoresSafeArea()
+      
+            VStack(alignment: .leading, spacing:8){
+                Spacer().frame(maxHeight: 150)
                 
-                
+              
                 VStack(alignment: .leading){
-                    HStack{
-                        Image(systemName: "book.pages.fill")
-                        Text(plant.plantName)
-                            .font(.title2)
-                            .bold()
-                    }.foregroundStyle(.brown)
-                    HStack{
-                        Image(systemName: "pencil.and.outline")
-                        Text("by \(plant.botaName)")
-                            .font(.subheadline)
-                            .bold()
-                    }.foregroundStyle(.secondary)
-                }
+                    Spacer().frame(maxHeight: 20)
+                    
+                    //content start
+                    VStack{
+                        Spacer().frame(maxHeight: 70)
+                        VStack(alignment: .leading){
+                            HStack{
+                                Text(plant.plantName)
+                                    .font(.title).bold()
+                                Spacer()
+                                HStack{
+                                    Image(systemName: "bookmark.fill")
+                                        .foregroundStyle(.blue.opacity(0.8))
+                                    Text(plant.catergory)
+                                }
+                                
+                            }
+                            Text(plant.botaName)
+                                .font(.subheadline)
+                            
+                            //schedule info
+                            HStack{
+                                VStack{
+                                
+                                    HStack{
+                                    Image(systemName: "sun.max.fill")
+                                        .foregroundStyle(Color("dateLabelColor"))
+                                    VStack(alignment: .leading){
+                                        Text("Sun light needed:")
+                                            .font(.subheadline)
+                                            .bold()
+                                        Text(plant.sunlight)
+                                            .font(.footnote)
+                                        }
+                                    }
+                                    Spacer()
+                                    HStack{
+                                        Image(systemName: "drop.halffull")
+                                            .foregroundStyle(Color("dateLabelColor"))
+                                        VStack(alignment: .leading){
+                                            Text("Water Schedule:")
+                                                .font(.subheadline)
+                                                .bold()
+                                            Text(plant.waterTime)
+                                                .font(.footnote)
+                                        }
+                                    }
+                                
+                              
+                                
+                            }.padding(5)
+                            Spacer()
+                            
+                                VStack{
+                                    
+                                    HStack{
+                                        Image(systemName: "thermometer.medium")
+                                            .foregroundStyle(Color("dateLabelColor"))
+                                        VStack(alignment: .leading){
+                                            Text("Temperature need:")
+                                                .font(.subheadline)
+                                                .bold()
+                                            Text(plant.sunlight)
+                                                .font(.footnote)
+                                        }
+                                    }
+                                    Spacer()
+                                    HStack{
+                                        Image(systemName: "apple.meditate")
+                                            .foregroundStyle(Color("dateLabelColor"))
+                                        VStack(alignment: .leading){
+                                            Text("Fertilize Schedule:")
+                                                .font(.subheadline)
+                                                .bold()
+                                            Text(plant.waterTime)
+                                                .font(.footnote)
+                                        }
+                                    }
+                                   
+                                }.padding(5)
+                            }.frame(maxHeight:100)
+                            
+                        }
+                        VStack(alignment: .leading){
+                            Text("Summary")
+                                .font(.headline)
+                                .padding(.bottom)
+                            Text(plant.catergory)
+                            Button("Edit Book"){
+                                plant.plantName = "New Name Here"
+                            }
+                        }.padding()
+                            .background(.gray.opacity(0.3))
+                            .cornerRadius(10)
+                            Spacer()
+                        
+                        
+                    }
+                    .frame(width: 360, height: 580)
+                    .border(.blue)
+                                
+                            
+                    }
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            UnevenRoundedRectangle(topLeadingRadius: 50, topTrailingRadius: 50).fill(.white)
+                        )
                 
-            }//END: Hstack
-            VStack{
-                Text("Summary")
-                    .font(.headline)
-                    .padding(.bottom)
-                
-                Text(plant.catergory)
-                Button("Edit Book"){
-                    plant.plantName = "New Name Here"
-                }
-            }
-            .padding()
-            .background(.gray.opacity(0.3))
-            .cornerRadius(10)
-            Spacer()
+         
+
+                }//End: mid to end VStack
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea(edges: .bottom)
             
-        }.padding()
-    }
+                
+               
+                    VStack{
+                        Image(plant.plantImage)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(maxWidth:300, maxHeight:200)
+                            .cornerRadius(15)
+                            .overlay(alignment: .topTrailing){
+                                
+                                Text(plant.status)
+                                    .fontWeight(.bold)
+                                    .font(.title3)
+                                    .padding(.horizontal, 20)
+                                    .foregroundStyle(.white)
+                                    .background(Color("healthy").opacity(0.7))
+                            }.shadow(color: .gray.opacity(0.5), radius: 5, x: 5, y: 5)
+                            .offset(y: -510)
+                    }//End: image floating vstack
+                
+                
+                
+                
+                
+                
+            }//End: Zstack
+            .scrollContentBackground(.hidden)
+                
+               
+                }
+            
+//            .background(Color("listBackground"))
+                    .navigationTitle("Edit Plant")
+                    .navigationBarTitleDisplayMode(.inline)
+                        .navigationBarItems(trailing: Button("Edit"){
+                            showEditPlant.toggle()
+                        })
+                        .sheet(isPresented: $showEditPlant){
+                            
+                        }content:{
+                            AddView(plant: $plant)
+                    }
+            
+
+                       //                .navigationBarItems(trailing: Button("Edit")){
+                       //                    showEditPlant.toggle()
+                       //                }).sheet(isPresented: $showEditPlant){
+                       //
+                       //                }content:{
+                       //                    EditPlantView(plant: $plant)
+                       //                }
+               }//End: nav
+
+           }
+
+       
 }
 
 #Preview {
